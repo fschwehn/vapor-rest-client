88abeeec84d5c01434bb39ec8ad3bcd66c0d27ec
Florian Schwehn <fschwehn@gmail.com>
2020-01-14 17:48:05 +0100

    Port ClientAuthMiddleware OAuthSession to Vapor 4

--- Sources/RestClient/Middleware/Auth/ClientAuthMiddleware.swift
+++ Sources/RestClient/Middleware/Auth/ClientAuthMiddleware.swift
@@ -7,8 +7,8 @@
 
 import Vapor
 
-//public enum ClientAuthMiddlewareError: Debuggable {
-//    
+public enum ClientAuthMiddlewareError: LocalizedError {
+    
 //    public var identifier: String {
 //        switch self {
 //        case .authenticationFailedAfterTooManyRetrials(_):
@@ -17,7 +17,7 @@ import Vapor
 //            return "ClientAuthMiddlewareError.tokenRefreshFailed"
 //        }
 //    }
-//    
+    
 //    public var reason: String {
 //        switch self {
 //        case .authenticationFailedAfterTooManyRetrials(let url):
@@ -27,101 +27,115 @@ import Vapor
 //            return "Token refresh failed calling URL '\(url)':\n\t\(reason.split(separator: "\n").joined(separator: "\n\t"))"
 //        }
 //    }
-//    
-//    case authenticationFailedAfterTooManyRetrials(url: URLRepresentable)
-//    case tokenRefreshFailed(url: URLRepresentable, response: Response)
-//    
-//}
-//
-//open class ClientAuthMiddleware<Session>: Middleware where Session: OAuthSession {
-//
-//    public let container: Container
-//    public let clientId: String
-//    public let clientSecret: String
-//    
-//    private(set) var accessTokenUrl: URLRepresentable
-//    private(set) var session: Session
-//    
-//    private var refreshTokenFuture: EventLoopFuture<String>?
-//    
-//    public init(container: Container,
-//                clientId: String,
-//                clientSecret: String,
-//                accessTokenUrl: URLRepresentable,
-//                session: Session)
-//    {
-//        self.container = container
-//        self.clientId = clientId
-//        self.clientSecret = clientSecret
-//        self.accessTokenUrl = accessTokenUrl
-//        self.session = session
-//    }
-//    
-//    open func respond(to request: Request, chainingTo next: Responder) throws -> EventLoopFuture<Response> {
-//        return try token(container: request)
-//            .flatMap({ (token) -> EventLoopFuture<Response> in
-//                return try self.respond(to: request, token: token, chainingTo: next, trialsLeft: 3)
-//            })
-//    }
-//    
-//    private func token(container: Container) throws -> EventLoopFuture<String> {
-//        if session.expiresAt < Date() {
-//            refreshTokenFuture = try refreshToken(container: container)
-//        }
-//        return refreshTokenFuture ?? container.eventLoop.future(session.accessToken)
-//    }
-//    
-//    private func respond(to request: Request, token: String, chainingTo next: Responder, trialsLeft: Int) throws -> EventLoopFuture<Response> {
-//        // check for max trials
-//        if trialsLeft == 0 {
-//            throw ClientAuthMiddlewareError.authenticationFailedAfterTooManyRetrials(url: request.http.url)
-//        }
-//        
-//        // set auth header
-//        request.http.headers.replaceOrAdd(name: .authorization, value: "Bearer \(token)")
-//        
-//        // send request
-//        return try next.respond(to: request)
-//            .flatMap({ (res) -> EventLoopFuture<Response> in
-//                // handle unauthorized status
-//                if res.http.status == .unauthorized {
-//                    return try self.refreshToken(container: request)
-//                        .flatMap({ (token) -> EventLoopFuture<Response> in
-//                            return try self.respond(to: request, token: token, chainingTo: next, trialsLeft: trialsLeft - 1)
-//                        })
-//                }
-//                
-//                return request.eventLoop.future(res)
-//            })
-//    }
-//    
-//    private func refreshToken(container: Container) throws -> EventLoopFuture<String> {
-//        if let pendingRefresh = refreshTokenFuture {
-//            return pendingRefresh
-//        }
-//        
-//        let newRefreshTokenFuture = try container.client()
-//            .post(accessTokenUrl, beforeSend: { (req) in
-//                let body = session.tokenRequestBody(clientId: clientId, clientSecret: clientSecret)
-//                try req.content.encode(body, as: .formData)
-//            })
-//            // validate response status
-//            .flatMap({ (res) -> EventLoopFuture<String> in
-//                guard res.http.status == .ok else {
-//                    throw ClientAuthMiddlewareError.tokenRefreshFailed(url: self.accessTokenUrl, response: res)
-//                }
-//                
-//                return try self.session
-//                    .update(with: res.content, on: self.container)
-//                    .map({ $0.accessToken })
-//            })
-//            .always({
-//                self.refreshTokenFuture = nil
-//            })
-//        
-//        refreshTokenFuture = newRefreshTokenFuture
-//        
-//        return newRefreshTokenFuture
-//    }
-//    
-//}
+    
+    case authenticationFailedAfterTooManyRetrials(url: URI)
+    case tokenRefreshFailed(url: URI, response: ClientResponse)
+    
+}
+
+open class ClientAuthMiddleware<Session>: RestClientMiddleware where Session: OAuthSession {
+
+    public let client: Client
+    public let clientId: String
+    public let clientSecret: String
+    public let eventLoop: EventLoop
+    
+    private(set) var accessTokenUrl: URI
+    private(set) var session: Session
+    
+    private var refreshTokenPromise: EventLoopPromise<String>?
+    
+    public init(client: Client,
+                clientId: String,
+                clientSecret: String,
+                accessTokenUrl: URI,
+                session: Session,
+                eventLoop: EventLoop)
+    {
+        self.client = client
+        self.clientId = clientId
+        self.clientSecret = clientSecret
+        self.accessTokenUrl = accessTokenUrl
+        self.session = session
+        self.eventLoop = eventLoop
+    }
+    
+    open func respond(to request: ClientRequest, chainingTo next: RestClientResponder) -> EventLoopFuture<ClientResponse> {
+        return token().flatMap { token in
+            return self.respond(to: request, token: token, chainingTo: next, trialsLeft: 3)
+        }
+    }
+    
+    private func token() -> EventLoopFuture<String> {
+        guard session.expiresAt < Date() else {
+            return refreshToken()
+        }
+        
+        return refreshTokenPromise?.futureResult ?? eventLoop.future(session.accessToken)
+    }
+    
+    private func respond(to request: ClientRequest, token: String, chainingTo next: RestClientResponder, trialsLeft: Int) -> EventLoopFuture<ClientResponse> {
+        // check for max trials
+        guard trialsLeft > 0 else {
+            return eventLoop.makeFailedFuture(ClientAuthMiddlewareError.authenticationFailedAfterTooManyRetrials(url: request.url))
+        }
+        
+        var request = request
+        
+        // set auth header
+        request.headers.replaceOrAdd(name: .authorization, value: "Bearer \(token)")
+        
+        // send request
+        return next.respond(to: request)
+            .flatMap({ response -> EventLoopFuture<ClientResponse> in
+                // handle unauthorized status
+                if response.status == .unauthorized {
+                    return self.refreshToken().flatMap { token in
+                        self.respond(to: request, token: token, chainingTo: next, trialsLeft: trialsLeft - 1)
+                    }
+                }
+                
+                return self.eventLoop.future(response)
+            })
+    }
+    
+    private func refreshToken() -> EventLoopFuture<String> {
+        switch refreshTokenPromise {
+        case .some(let existing):
+            return existing.futureResult
+        default:
+            let newRefreshTokenPromise = eventLoop.makePromise(of: String.self)
+            refreshTokenPromise = newRefreshTokenPromise
+            
+            client.post(accessTokenUrl, beforeSend: { (req) in
+                let body = session.tokenRequestBody(clientId: clientId, clientSecret: clientSecret)
+                try req.content.encode(body, as: .formData)
+            }).whenComplete { result in
+                switch result {
+                case .failure(let error):
+                    newRefreshTokenPromise.fail(error)
+                    break
+                case .success(let response):
+                    guard response.status == .ok else {
+                        newRefreshTokenPromise.fail(ClientAuthMiddlewareError.tokenRefreshFailed(url: self.accessTokenUrl, response: response))
+                        return
+                    }
+                    
+                    self.session.update(with: response.content, on: self.eventLoop).whenComplete { result in
+                        switch result {
+                        case .failure(let error):
+                            newRefreshTokenPromise.fail(error)
+                        case .success(let session):
+                            newRefreshTokenPromise.succeed(session.accessToken)
+                        }
+                    }
+                }
+            }
+            
+            newRefreshTokenPromise.futureResult.whenComplete { _ in self.refreshTokenPromise = nil }
+            
+            return newRefreshTokenPromise.futureResult
+        }
+    }
+    
+}
